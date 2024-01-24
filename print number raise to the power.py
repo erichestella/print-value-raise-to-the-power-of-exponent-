ff50500f5d5902e8a9c0c46713fb0bb99f5f6e5c
erichestella <erichestella05@gmail.com>
2024-01-24 23:24:29 +0800

    this str display the number tjat just input

--- print number raise to the power.py
+++ print number raise to the power.py
@@ -1,6 +1,9 @@
 #added some title
-print('\nDISPLAY THE TOTAL NUMBER TAHT IS RAISED TO THE POWER.\n')
+print('\nDISPLAYING THE TOTAL NUMBER THAT IS RAISED TO THE POWER.\n')
 
 #variables to input the number and the power
 numerical_digit = int(input('ENTER NUMBER: '))
 exponential_integers = int(input('\nINPUT THE POWER: '))
+
+#it display the number you just input
+numerical_define = str(numerical_digit)
