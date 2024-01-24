19af7ca0dee316d3fa3c01d347e9a237eb49133b
erichestella <erichestella05@gmail.com>
2024-01-24 23:19:49 +0800

    variables to input the number you wand and its power

--- print number raise to the power.py
+++ print number raise to the power.py
@@ -1,2 +1,6 @@
 #added some title
 print('\nDISPLAY THE TOTAL NUMBER TAHT IS RAISED TO THE POWER.\n')
+
+#variables to input the number and the power
+numerical_digit = int(input('ENTER NUMBER: '))
+exponential_integers = int(input('\nINPUT THE POWER: '))
